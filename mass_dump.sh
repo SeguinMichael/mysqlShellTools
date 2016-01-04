8cab47d9a8f4cf0f60247fa2da722d3b40c8c18a
Seguin Michael <seguin.michael@gmail.com>
2016-01-04 17:53:35 +0100

    [REFACTOR] now uses getopt [EVO] it is now possible to dump some tables instead of complete databases (identical of -B mysqldump option)

--- mass_dump.sh
+++ mass_dump.sh
@@ -1,34 +1,88 @@
 #!/bin/bash
 
-#####
-#
-# Usage : ./mass_dump.sh "-h server -uroot -psecret" database1 database2 database3
-#
-#####
+function usage() {
+	echo "
+	Usage :
+		./mass_dump.sh [OPTIONS] extra arguments
+			-d DATA_PATH <= directory to the data storage
+			-n MAX_THREAD <= Multithreading mode
+			-s \"-h server1 -uroot\" <= server1
+			-B <= use extra arguments as database list instead of database_name and tables (mysqldump syntax)
+"
 
-CONNECTION_STRING=$1
-shift
-DATABASE_LIST=$@
+}
 
 DATE_DEBUT=$(date)
-MAX_THREAD=4
+
+MAX_THREAD=1
 DATA_PATH="/data/sqlDump"
+MULTIDATABASES="false"
+
+while getopts "hd:n:Bs:" option
+do
+	case $option in
+		h)
+			usage
+			exit
+			;;
+		d)
+			DATA_PATH=$OPTARG
+			;;
+		n)
+			MAX_THREAD=$OPTARG
+			;;
+		B)
+			MULTIDATABASES="true"
+			;;
+		s)
+			CONNECTION_STRING=$OPTARG
+			;;
+		\?)
+			exit 1
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
+echo Options activated :
+echo ___ data path : $DATA_PATH
+echo ___ max threads : $MAX_THREAD
+echo ___ multi databases : $MULTIDATABASES
+
+if [ "$MULTIDATABASES" = "true" ]
+then
+	DATABASE_LIST=$@
+else
+	DATABASE=$1
+	shift
+	TABLE_LIST=$@
+fi
 
 mkdir -p $DATA_PATH
 
-if [ -z "$CONNECTION_STRING" -o -z "$DATABASE_LIST" ]
+if [ -z "$DATA_PATH" -o -z "$MAX_THREAD" -o -z "$MULTIDATABASES" -o -z "$CONNECTION_STRING" ]
 then
-	echo "Usage : ./mass_dump.sh '-h server -uroot -psecret' database1 database2 database3"
-	exit;
+	usage
+	exit
 fi
 
 MYSQLDUMP_STRUCT="mysqldump -d -R --triggers -C --skip-disable-keys --skip-add-locks --skip-lock-tables --single-transaction $CONNECTION_STRING"
 MYSQLDUMP_DATA="mysqldump --replace -t --skip-triggers -C --skip-disable-keys --skip-add-locks --skip-lock-tables --single-transaction $CONNECTION_STRING"
 
 echo Dumping schema
-parallel --retries 5 --eta --progress --jobs $MAX_THREAD "$MYSQLDUMP_STRUCT {1} | lz4 > ${DATA_PATH}/{1}_struct.sql.lz4" ::: $DATABASE_LIST
+if [ "$MULTIDATABASES" = "true" ]
+then
+	parallel --retries 5 --eta --progress --jobs $MAX_THREAD "$MYSQLDUMP_STRUCT {1} | lz4 > ${DATA_PATH}/{1}_struct.sql.lz4" ::: $DATABASE_LIST
+else
+	parallel --retries 5 --eta --progress --jobs $MAX_THREAD "$MYSQLDUMP_STRUCT $DATABASE {1} | lz4 > ${DATA_PATH}/${DATABASE}:{1}_struct.sql.lz4" ::: $TABLE_LIST
+fi
 echo Dumping data
-parallel --retries 5 --eta --progress --jobs $MAX_THREAD "$MYSQLDUMP_DATA {1} | lz4 > ${DATA_PATH}/{1}_data.sql.lz4" ::: $DATABASE_LIST
+if [ "$MULTIDATABASES" = "true" ]
+then
+	parallel --retries 5 --eta --progress --jobs $MAX_THREAD "$MYSQLDUMP_DATA {1} | lz4 > ${DATA_PATH}/{1}_data.sql.lz4" ::: $DATABASE_LIST
+else
+	parallel --retries 5 --eta --progress --jobs $MAX_THREAD "$MYSQLDUMP_DATA $DATABASE {1} | lz4 > ${DATA_PATH}/${DATABASE}:{1}_data.sql.lz4" ::: $TABLE_LIST
+fi
 
 echo "Time stats :"
 echo "FROM $DATE_DEBUT TO $(date)"
